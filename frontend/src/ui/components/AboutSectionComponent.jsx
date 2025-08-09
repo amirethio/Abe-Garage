306162b7c93b2cd72da989f38aacd66ae0ca1623
amirethio <amiraliyimer@gmail.com>
2025-08-09 22:53:16 +0300

    home page  cocomponents

---
+++ frontend/src/ui/components/AboutSectionComponent.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import vban1 from "../../assets/templateImg/custom/misc/vban1.jpg";
+import vban2 from "../../assets/templateImg/custom/misc/vban2.jpg";
+
+
+
+function AboutSectionComponent() {
+  return (
+    <section class="about-section">
+            <div class="auto-container">
+                <div class="row">
+                    <div class="col-lg-5">
+                        <div class="image-box">
+                            <img src={vban1} alt=""/>
+                            <img src={vban2} alt=""/>
+                            <div class="year-experience" data-parallax='{"y": 30}'><strong>17</strong> years <br/>
+                                Experience </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-7 pl-lg-5">
+                        <div class="sec-title">
+                            <h5>Welcome to Our workshop</h5>
+                            <h2>We have 24 years experience</h2>
+                            <div class="text">
+                                <p>Bring to the table win-win survival strategies to ensure proactive domination. At the
+                                    end of the day, going forward, a new normal that has evolved from generation X is on
+                                    the runway heading towards a streamlined cloud solution. User generated content in
+                                    real-time will have multiple touchpoints for offshoring.</p>
+                                <p>Capitalize on low hanging fruit to identify a ballpark value added activity to beta
+                                    test. Override the digital divide with additional clickthroughs from DevOps.
+                                    Nanotechnology immersion along the information highway will close the loop on
+                                    focusing.</p>
+                            </div>
+                            <div class="link-btn mt-40"><a href="about.html"
+                                    class="theme-btn btn-style-one style-two"><span>About Us <i
+                                            class="flaticon-right"></i></span></a></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+  );
+}
+
+export default AboutSectionComponent;
--- frontend/src/ui/pages/Home.jsx
+++ frontend/src/ui/pages/Home.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
+import Video from "../components/Video/Video";
+import AboutSectionComponent from "../components/AboutSectionComponent";
+import OurServiceComponent from "../components/OurServiceComponent";
 
 function Home() {
   return (
-    <div>Home</div>
-  )
+    <>
+      <Video />
+      <AboutSectionComponent/>
+      <OurServiceComponent/>
+      <div>Home</div>
+    </>
+  );
 }
 
-export default Home
+export default Home;
